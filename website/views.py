9e96003dcaa3e37b6fa1cebfb134ba8a60549465
akshigupta361 <guptaakshi361@gmail.com>
2020-03-27 23:45:31 +0530

    Finalising the add page

--- website/views.py
+++ website/views.py
@@ -7,8 +7,8 @@ def home(request):
 
 def add(request):
     from random import randint
-    num_1= randint(0,10)
-    num_2= randint(0,10)
+    num_1= randint(0,100)
+    num_2= randint(0,100)
 
     if request.method == "POST":
         answer= request.POST['answer']
@@ -37,7 +37,35 @@ def add(request):
     })
 
 def subtract(request):
-    return render(request, 'subtract.html', {})
+    from random import randint
+    num_1= randint(0,10)
+    num_2= randint(0,10)
+
+    if request.method == "POST":
+        answer= request.POST['answer']
+        old_num_1= request.POST['old_num_1']
+        old_num_2= request.POST['old_num_2']
+
+        correct_answer= int(old_num_1)-int(old_num_2)
+
+        if int(answer)==correct_answer:
+            my_answer="Correct!"
+            color="success"
+        else:
+            my_answer="Incorrect! " + old_num_1 + " - " +  old_num_2 + " is " +  str(correct_answer) + " not " + answer
+            color="danger"
+        return render(request, 'subtract.html', {
+            'answer':answer,
+            'my_answer':my_answer,
+            'num_1':num_1,
+            'num_2':num_2,
+            'color':color,
+            })
+
+    return render(request, 'subtract.html', {
+        'num_1':num_1,
+        'num_2':num_2,
+    })
 
 def multiply(request):
     return render(request, 'multiply.html', {})
