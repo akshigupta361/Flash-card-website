7811bcd988f3005cf79d8be1047b5caadea82fe1
akshigupta361 <guptaakshi361@gmail.com>
2020-03-27 22:41:15 +0530

    Showing correct output

--- website/views.py
+++ website/views.py
@@ -12,7 +12,21 @@ def add(request):
 
     if request.method == "POST":
         answer= request.POST['answer']
-        return render(request, 'add.html', {'answer':answer})
+        old_num_1= request.POST['old_num_1']
+        old_num_2= request.POST['old_num_2']
+
+        correct_answer= int(old_num_1)+int(old_num_2)
+
+        if int(answer)==correct_answer:
+            my_answer="Correct!"
+        else:
+            my_answer="Incorrect! " + old_num_1 + " + " +  old_num_2 + " is " +  str(correct_answer)
+        return render(request, 'add.html', {
+            'answer':answer,
+            'my_answer':my_answer,
+            'num_1':num_1,
+            'num_2':num_2,
+            })
 
     return render(request, 'add.html', {
         'num_1':num_1,
